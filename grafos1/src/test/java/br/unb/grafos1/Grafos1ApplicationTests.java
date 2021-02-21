79952de358a4b0ad69ec5a42e55e4951f497e172
Lucas Lopes <l.lopes.fga@gmail.com>
2021-02-21 20:51:09 -0300

    back-end

---
+++ grafos1/src/test/java/br/unb/grafos1/Grafos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unb.grafos1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grafos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
